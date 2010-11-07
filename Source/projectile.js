27500b8acee4f5db264e3f21ad6e576a53a1ccfd
Sergio <dev@panaghia.it>
2010-11-07 17:15:55 +0100

    rewritten

--- Source/projectile.js
+++ Source/projectile.js
@@ -1,96 +1,42 @@
-var Projectile = new Class({
-	Implements: [Options, Events],
-	options:
-	{
-		inity: null,
-		initx: null,
-		v0x: null,
-		v0y: null,
-		posx: 0,
-		posy: 0,
-		radius: 15,
-		vel: 40,
-		angle:45,
-		context: null,
-		frameCount: 0,
-		gravity: 9.8,
-		moving: false,
-		start: null
-	},
-	initialize: function(options)
-	{
-		this.setOptions(options);
-		this.options.v0x = this.options.vel * Math.cos(this.options.angle * Math.PI/180);
-		this.options.v0y = this.options.vel * Math.sin(this.options.angle * Math.PI/180);
-		this.options.inity = this.options.posy;
-		this.options.initx = this.options.posx;
-		
-		this.render();
-	},
-	render: function()
-	{
-		if(this.options.moving)
-		{
-			var currentTime = new Date().getTime()/100;
-			var delta = currentTime - this.options.start;
-			this.options.frameCount = delta;
-		}
-		else
-			this.options.frameCount = 0;
-		
-		ctx.beginPath();
-			ctx.fillStyle = "rgba(0, 200, 0, 0.6)";
-			ctx.arc(this.options.posx,this.options.posy,this.options.radius,0,Math.PI*2,true);
-			ctx.fill();
-			ctx.stroke();
-		ctx.closePath();
-		if(this.options.moving)
-		{
-		
-			this.options.posx = this.options.initx + this.options.v0x * this.options.frameCount;
+var pro = {
+				x:50,
+				y:380,
+				r:15,
+				v:80,
+				theta: 45
+				};
+				
+				var canvas = document.getElementById('surface');
+				var ctx = canvas.getContext('2d');
+				
+				var frameCount = 0;
+				var v0x = pro.v * Math.cos(pro.theta * Math.PI/180);
+				var v0y = pro.v * Math.sin(pro.theta * Math.PI/180);
+				var startX = pro.x;
+				var startY = pro.y;
+				var g = 9.8;
+				setInterval(function()
+				{
+					//smooth clear
+					ctx.save();
+						ctx.fillStyle = "rgba(0, 0, 0, .3)";
+						ctx.fillRect(0, 0, canvas.width, canvas.height);
+					ctx.restore();
+					
+					if(pro.y<canvas.height - pro.r && pro.x < canvas.width - pro.r)
+					{
+						pro.y = startY - ( v0y * frameCount - (1/2 * g * Math.pow(frameCount,2)) );
+						pro.x = startX + v0x * frameCount;
+					}
 						
-			var py =  (this.options.v0y * this.options.frameCount) - (1/2*this.options.gravity*(this.options.frameCount*this.options.frameCount));
-			this.options.posy = this.options.inity - py;		
-			
-			var res = new Number(py);
-			
-			if(this.options.posy >= document.id('canv').getSize().y - this.options.radius) //this.options.inity && this.options.frameCount>0)
-			{
-				this.stop();
-				this.options.posy = document.id('canv').getSize().y - this.options.radius
-			}
-			
-			var currentVY = this.options.vel * Math.sin(this.options.angle * Math.PI/180) - this.options.gravity * this.options.frameCount;
-			
-			/*var dm =  'posx: '+this.options.posx+
-						'<br/>posy: '+this.options.posy+
-						'<br/>time: '+this.options.frameCount+
-						'<br/>vel: '+this.options.v0x+
-						'<br/>vely: '+currentVY;
+					ctx.save();
+						ctx.beginPath();
+						ctx.fillStyle = "rgba(0, 200, 0, 0.6)";
+						ctx.arc(pro.x,pro.y,pro.r,0,Math.PI*2,true);
+						ctx.fill();
+						ctx.stroke();
+						ctx.closePath();
+					ctx.restore();
+					frameCount+=.1;
 						
-			document.id('debug').set('html', dm);*/
-
-		}
-		
-	},
-	start: function()
-	{
-		this.options.moving = true;
-		var time = new Date();
-		this.options.start =  time.getTime()/100;
-	},
-	stop: function()
-	{
-		this.options.moving = false;
-	},
-	setVelocity: function(newVel)
-	{
-		this.options.vel = newVel;
-		this.options.v0x = this.options.vel * Math.cos(this.options.angle * Math.PI/180);
-		this.options.v0y = this.options.vel * Math.sin(this.options.angle * Math.PI/180); 
-	},
-	setAngle: function(newAngle)
-	{
-		this.options.angle = newAngle;
-	}
-});
+				}, 1000 / 77);
